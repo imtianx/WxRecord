7f61a299d7fac10e6fa026ef9813beff7c2eaf1c
imtianx <imtianx@gmail.com>
2019-03-04 11:28:46 +0800

    debug net;add msg filter;add setting ui;

---
+++ app/src/main/java/cn/imtianx/wxrecord/ui/SettingActivity.kt
@@ -0,0 +1,13 @@
+package cn.imtianx.wxrecord.ui
+
+/**
+ * <pre>
+ *     @desc:
+ * </pre>
+ * @fileName SettingActivity
+ * @author 奚岩
+ * @email imtianx@gmail.com
+ * @date 2019/3/4 11:23 AM
+ */
+class SettingActivity {
+}
